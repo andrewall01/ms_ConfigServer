bbe8f8d3d813445464dcd13b1fb536a7414e3f11
Andres Llontop <andresllontop01@gmail.com>
2022-07-14 16:29:37 -0500

    Se entrega proyecto1 -config server

---
+++ src/test/java/com/project/ms/config/server/MsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ms.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
